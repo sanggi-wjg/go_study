12f1483d6b21fa6d0eb27dd1be14f9eb368c08a8
sanggi-wjg <girr311@naver.com>
2021-08-22 12:45:11 +0900

    2021 08 22

--- main.go
+++ main.go
@@ -41,10 +41,15 @@ func main() {
 	totalPages := getTotalPages()
 	// fmt.Println("Total Pages:", totalPages)
 	jobs := []ScrappedJobResult{}
+	c := make(chan []ScrappedJobResult)
 
 	for i := 0; i < totalPages; i++ {
-		scrappedJobs := getPage(i)
-		jobs = append(jobs, scrappedJobs...)
+		go getPage(i, c)
+		// jobs = append(jobs, scrappedJobs...)
+	}
+	for i := 0; i < totalPages; i++ {
+		job := <-c
+		jobs = append(jobs, job...)
 	}
 
 	// fmt.Println(jobs)
@@ -70,18 +75,6 @@ func resultToCSV(jobs []ScrappedJobResult) {
 	}
 }
 
-func checkErr(err error) {
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
-
-func checkStatusCode(res *http.Response) {
-	if res.StatusCode != 200 {
-		log.Fatalln("request failed with ", res.StatusCode, res.Status)
-	}
-}
-
 func requestPage(requestURL string) *http.Response {
 	res, err := http.Get(baseURL)
 	checkErr(err)
@@ -90,23 +83,29 @@ func requestPage(requestURL string) *http.Response {
 	return res
 }
 
-func getPage(page int) []ScrappedJobResult {
+func getPage(page int, mainc chan []ScrappedJobResult) {
 	jobs := []ScrappedJobResult{}
+	c := make(chan ScrappedJobResult)
+
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
 	res := requestPage(pageURL)
 
 	defer res.Body.Close() // io 여서 close() 해줘야 함
 	doc, _ := goquery.NewDocumentFromReader(res.Body)
 
-	doc.Find("#mosaic-provider-jobcards > a").Each(func(i int, card *goquery.Selection) {
-		job := scrapJob(card)
-		jobs = append(jobs, *job)
+	jobCards := doc.Find("#mosaic-provider-jobcards > a")
+	jobCards.Each(func(i int, card *goquery.Selection) {
+		go scrapJob(card, c)
 	})
 
-	return jobs
+	for i := 0; i < jobCards.Length(); i++ {
+		job := <-c
+		jobs = append(jobs, job)
+	}
+	mainc <- jobs
 }
 
-func scrapJob(card *goquery.Selection) *ScrappedJobResult {
+func scrapJob(card *goquery.Selection, c chan ScrappedJobResult) {
 	id, _ := card.Attr("data-jk")
 	title := card.Find(".jobTitle > span").Text()
 	companyName := card.Find(".company_location > pre > span").Text()
@@ -115,7 +114,8 @@ func scrapJob(card *goquery.Selection) *ScrappedJobResult {
 	jobURL := "https://kr.indeed.com/%EC%B1%84%EC%9A%A9%EB%B3%B4%EA%B8%B0?jk=" + id
 	// fmt.Println(id, title, companyName, location, salary)
 
-	return NewScrappedJobResult(jobURL, id, title, companyName, location, salary)
+	// return NewScrappedJobResult(jobURL, id, title, companyName, location, salary)
+	c <- ScrappedJobResult{pageURL: jobURL, id: id, title: title, name: companyName, location: location, salary: salary}
 }
 
 func getTotalPages() int {
@@ -131,4 +131,16 @@ func getTotalPages() int {
 	})
 
 	return pages
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkStatusCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("request failed with ", res.StatusCode, res.Status)
+	}
 }
